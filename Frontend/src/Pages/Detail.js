ae649c1d6693d9b70e641c34e98648145427b64d
Neøw <tommy.riquet@hotmail.com>
2022-03-30 11:07:37 +0200

    Adding description to detail page

--- Frontend/src/Pages/Detail.js
+++ Frontend/src/Pages/Detail.js
@@ -34,11 +34,7 @@ const Detail = () => {
                 </Col>
                 <Col md='6' style={{marginTop:"5%"}}>
                     <h4 style={{textDecoration:"underline", margin: "15px  0"}}>Description</h4>
-                    <p>La cantatrice Bianca Castafiore est arrêtée par la police du San Theodoros pendant sa tournée sud américaine.
-                         Elle est accusée d’avoir comploté contre le pouvoir. Le général Tapioca dit avoir aussi des preuves de
-                          l’implication de Tintin, du capitaine Haddock et du professeur Tournesol. Il invite nos trois héros à venir
-                           s’expliquer, ce qu’ils font. Mais il s’agit en fait d’un piège fomenté par un de leurs anciens ennemis pour
-                            les éliminer.
+                    <p>{book.description}
                     </p>
                     <h4 style={{margin: "15px  0"}}>Prix : <span style={{color: "red"}}>7.50€</span></h4>
                     <Button variant="warning" style={{minWidth:"100%", fontSize:"20px", fontWeight:"bolder"}}>Réserver le livre</Button>
