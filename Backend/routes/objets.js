f0eb7c5d0b16d863c2006d45f4bd69d04fcaa1b1
Marina <m.quadu@students.ephec.be>
2022-04-04 20:35:36 +0200

    userstory objet find

--- Backend/routes/objets.js
+++ Backend/routes/objets.js
@@ -14,4 +14,60 @@ router.get('/', function(req, res, next) {
   })
 });
 
+router.get('/statuette', function(req, res, next) {
+  /*Requête à la DB*/
+  Objet.find({type:'statuette'},(err, DBres)=>{
+      if (err) return handleError(err);
+      res.send(DBres);
+  })
+});
+
+router.get('/poster', function(req, res, next) {
+  /*Requête à la DB*/
+  Objet.find({type:'poster'},(err, DBres)=>{
+      if (err) return handleError(err);
+      res.send(DBres);
+  })
+});
+
+router.get('/montre', function(req, res, next) {
+  /*Requête à la DB*/
+  Objet.find({type:'montre'},(err, DBres)=>{
+      if (err) return handleError(err);
+      res.send(DBres);
+  })
+});
+
+router.get('/vaisselle', function(req, res, next) {
+  /*Requête à la DB*/
+  Objet.find({type:'vaisselle'},(err, DBres)=>{
+      if (err) return handleError(err);
+      res.send(DBres);
+  })
+});
+
+router.get('/jeudecarte', function(req, res, next) {
+  /*Requête à la DB*/
+  Objet.find({type:'jeudecarte'},(err, DBres)=>{
+      if (err) return handleError(err);
+      res.send(DBres);
+  })
+});
+
+router.get('/cartepostale', function(req, res, next) {
+  /*Requête à la DB*/
+  Objet.find({type:'cartepostale'},(err, DBres)=>{
+      if (err) return handleError(err);
+      res.send(DBres);
+  })
+});
+
+router.get('/gadget', function(req, res, next) {
+  /*Requête à la DB*/
+  Objet.find({type:'gadget'},(err, DBres)=>{
+      if (err) return handleError(err);
+      res.send(DBres);
+  })
+});
+
 module.exports = router;
