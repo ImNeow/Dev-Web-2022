fcb4e4c9ae808dbc3cbe6a3f059114366d86cefc
Kevin Keurvels <k.keurvels@students.ephec.be>
2022-04-04 15:04:10 +0200

    Changement model

--- Backend/models/books.model.js
+++ Backend/models/books.model.js
@@ -5,7 +5,8 @@ const bookSchema = new mongoose.Schema({
     /*Ce Schéma est utilisé lors de l'envoie ou la récupération de livre dans le DB Mongo */
     type: { type : String},
     name: { type : String},
-    link: { type : String}
+    link: { type : String},
+    description: { type : String}
 })
 
 module.exports = mongoose.model('Book', bookSchema);
