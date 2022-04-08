75ba368e1db0208a3e5032f8212316226b2d7b61
TommyRiquet <tommy.riquet@hotmail.com>
2022-04-08 18:38:04 +0200

    Connexion front-back Filtre

--- Frontend/src/Pages/Bedetheque.js
+++ Frontend/src/Pages/Bedetheque.js
@@ -9,30 +9,32 @@ import marsup from '../Assets/Images/marsupilami-down.png'
 import dbz from '../Assets/Images/dbzcloud.png'
 import batsignal from '../Assets/Images/batsignal.png'
 
-
 const Animation = {'BD':["anim-marsup",marsup],'Manga':["anim-dbz",dbz],'Comic':["anim-signalbat",batsignal]};
+
 
 const Bedetheque = (props) => {
   const [listBD, setlistBD] = useState([])
   const [nbrBookPerRow, setnbrBookPerRow] = useState(5); /*Min : 1 , Max : 6*/
+  const [filtre, setFiltre] = useState('alphaAsc');
   const type= props.type
 
   const animationClassname = Animation[type][0]
   const animationSrc = Animation[type][1]
+  
 
   useEffect(()=>{
     /* Cette fonction fait un appel à l'API pour récuperer les objets des BDs par rapport à leurs types
     PRE : /
     POST : /
     */
-    fetch("/books/"+type).then(res =>{
+    fetch("/books/"+type+'/'+filtre).then(res =>{
       if(res.ok){
         return res.json()
       }
     }).then(jsonResponse => {
       setlistBD(jsonResponse)
     })
-  },[])
+  })
 
 
   useEffect(() => {
@@ -44,41 +46,44 @@ const Bedetheque = (props) => {
   
 
     return<>
-    <Media query="(min-width: 992px) and (min-height : 600px)" render={() =>(<div className="animContainer"><img className={animationClassname} src={animationSrc} ></img></div>)}/>
+      <Media query="(min-width: 992px) and (min-height : 600px)" render={() =>(<div className="animContainer"><img className={animationClassname} src={animationSrc} ></img></div>)}/>
 
-    <div className="ListContent">
-              
-                
-              <Container className="containerContent">
-              <select className="filter">
-              <option selected>Nom: A-Z</option>
-              <option>Nom: Z-A</option>
-              <option>Prix: par ordre croissant</option>
-              <option>Prix: par ordre décroissant</option>
+      <div className="ListContent"> 
+        
+        <Container className="containerContent">   
+          <Row>
+            <Col>
+            <select id="filterSelect" defaultValue={"alphaAsc"} className="filter" onChange={e => setFiltre(e.target.selectedOptions[0].value)}>
+                <option value="alphaAsc" >Nom: A-Z</option>
+                <option value="alphaDesc" >Nom: Z-A</option>
+                <option value="priceAsc" >Prix: par ordre croissant</option>
+                <option value="priceDesc" >Prix: par ordre décroissant</option>
               </select>
-                <Row xs={1} md={nbrBookPerRow}>
-                  {
-                    listBD.map((myBD,index) => {
-                      let nameBD= " ";
-                      nameBD = myBD.name;
-                      return (
-                        <Col key={"Col"+index} style={{marginBottom:'5px'}}>
-                          <a href={'/detail/books/'+myBD._id} style={{textDecoration:'none'}}>
-                            <Card key={myBD._id}>
-                              <Card.Img variant="top" src={myBD.link}/>
-                              <Card.Body style={{backgroundColor:'hsl(52, 97%, 55%)'}}>
-                                <Card.Title style={{minHeight:"2em",fontSize:"20px",color:'black'}}>{nameBD}</Card.Title>
-                                <Card.Text  className="priceBD">{myBD.price}€</Card.Text>
-                              </Card.Body>
-                            </Card>
-                          </a>
-                        </Col>
-                      )
-                    })  
-                  }
-                </Row>
-              </Container>
-            </div>
-            </> 
+            </Col>
+          </Row>
+          <Row xs={1} md={nbrBookPerRow}>
+            {
+              listBD.map((myBD,index) => {
+                let nameBD= " ";
+                nameBD = myBD.name;
+                return (
+                  <Col key={"Col"+index} style={{marginBottom:'5px'}}>
+                    <a href={'/detail/books/'+myBD._id} style={{textDecoration:'none'}}>
+                      <Card key={myBD._id}>
+                        <Card.Img variant="top" src={myBD.link}/>
+                        <Card.Body style={{backgroundColor:'hsl(52, 97%, 55%)'}}>
+                          <Card.Title style={{minHeight:"2em",fontSize:"20px",color:'black'}}>{nameBD}</Card.Title>
+                          <Card.Text  className="priceBD">{myBD.price}€</Card.Text>
+                        </Card.Body>
+                      </Card>
+                    </a>
+                  </Col>
+                )
+              })  
+            }
+          </Row>
+        </Container>
+      </div>
+      </> 
     }
     export default Bedetheque
