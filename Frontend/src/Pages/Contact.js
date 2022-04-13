c348e2a2d18442522897c8d7c630b88c6276f98b
Kevin Keurvels <k.keurvels@students.ephec.be>
2022-04-13 17:41:30 +0200

    mail

--- Frontend/src/Pages/Contact.js
+++ Frontend/src/Pages/Contact.js
@@ -1,20 +1,27 @@
 import { Container,  Row, Col, Form, Button } from 'react-bootstrap'
 import { useState } from 'react';
+import emailjs from 'emailjs-com';
+import { useRef } from 'react';
+
 
 const Contact = () => {
-    const [validated, setValidated] = useState(false);  
+    const form = useRef();
     
     const handleSubmit = (event) => {
         /*Cette fonction vérifie si le champ est rempli
         PRE : l'évènement qui envoie le formulaire
         POST : /
         */ 
-        const form = event.currentTarget;
-        if (form.checkValidity() === false) {
             event.preventDefault();
             event.stopPropagation();
-        }
-        setValidated(true);
+            emailjs.sendForm('service_rse51yv', 'template_13on8q2', form.current, 'JioyY1I1MU-1KwY60')
+                .then(function(response) {
+                console.log('SUCCESS!', response.status, response.text);
+                }, function(error) {
+                console.log('FAILED...', error);
+                });
+            event.target.reset();
+
     };
 
     return(
@@ -26,11 +33,11 @@ const Contact = () => {
                     <h2>Contactez-Nous</h2>
                 </Col>
             </Row>
-            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form ref={form} onSubmit={handleSubmit}>
                 <Row className="justify-content-md-center mb-4">
                     <Col xs lg="3">
                         <Form.Group  controlId="validationCustom01">
-                            <Form.Control required type="text" placeholder="Nom"/>
+                            <Form.Control required type="text" placeholder="Nom" name="form_name"/>
                             <Form.Control.Feedback type="invalid">
                             Veuillez indiquer un nom de référence
                             </Form.Control.Feedback>
@@ -38,7 +45,7 @@ const Contact = () => {
                     </Col>
                     <Col xs lg="3">
                         <Form.Group  controlId="validationCustom02">
-                            <Form.Control required type="email" placeholder="Email"/>
+                            <Form.Control required type="email" placeholder="Email" name="form_email"/>
                             <Form.Control.Feedback type="invalid">
                             Veuillez indiquer une adresse e-mail valide
                             </Form.Control.Feedback>
@@ -48,7 +55,7 @@ const Contact = () => {
                 <Row className="justify-content-md-center mb-4">
                     <Col xs lg="6">
                         <Form.Group  controlId="validationCustom03">
-                            <Form.Control required type="text" placeholder="Sujet"/>
+                            <Form.Control required type="text" placeholder="Sujet" name="form_subject"/>
                             <Form.Control.Feedback type="invalid">
                             Veuillez indiquer un titre à votre message
                             </Form.Control.Feedback>
@@ -58,7 +65,7 @@ const Contact = () => {
                 <Row className="justify-content-md-center">
                 <Col xs lg="6">
                     <Form.Group className="mb-3" controlId="Mail_Message">
-                        <Form.Control required placeholder="Message" as="textarea" rows={3} />
+                        <Form.Control required placeholder="Message" as="textarea" rows={3}  name="form_message"/>
                         <Form.Control.Feedback type="invalid">
                             Veuillez indiquer votre message
                         </Form.Control.Feedback>
