f205833e2a3f8247f22b4188e383e9155ce171ee
Marina <m.quadu@students.ephec.be>
2022-04-22 21:31:17 +0200

    image vitrine pour l'accueil

--- Frontend/src/Pages/Accueil.js
+++ Frontend/src/Pages/Accueil.js
@@ -1,6 +1,17 @@
+import { Row, Col , Container } from "react-bootstrap"
+import image_vitrine from '../Assets/Images/vitrine.png'
+
 const Accueil = () => {
-return <div>
-Accueil
-</div>
-}
+    return (
+        <div class="Accueil">
+        <Container>
+            <Row className="justify-content-center">
+                <Col md="auto">
+                    <img src={image_vitrine} alt="devanture du magasin" height={804} width={1148}/>
+                </Col>
+            </Row>
+        </Container>
+        </div>
+    );
+ }
 export default Accueil
